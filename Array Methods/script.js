c660ff5a843cbe665e091aab9c90299e976b93fa
bimpe <adeyinminuadebimpe@gmail.com>
2023-02-07 15:51:16 +0100

    concat

--- Array Methods/script.js
+++ Array Methods/script.js
@@ -31,7 +31,7 @@ console.log(foodAvailability);
 
 // ARRAY METHODS
 
-//To string
+
 
 const rainbowColours = [
   "red",
@@ -43,7 +43,7 @@ const rainbowColours = [
   "violet",
 ];
 
-
+//To string
 document.querySelector("#txt1").innerHTML = rainbowColours.toString();
 
 // Join 
@@ -51,7 +51,50 @@ document.querySelector("#txt2").innerHTML = rainbowColours.join("/");
 
 // pop
 document.querySelector("#txt3").innerHTML = rainbowColours.pop();
+document.querySelector("#txt4").innerHTML = `POP = ${rainbowColours}`
+
 
 //push
-document.querySelector("#txt4").innerHTML = rainbowColours.push();
+document.querySelector("#txt5").innerHTML = rainbowColours.push("PINK");
+document.querySelector("#txt6").innerHTML = `PUSH = ${rainbowColours} `
 
+
+//shift
+document.getElementById("txt7").innerHTML = rainbowColours.shift();
+document.getElementById("txt8").innerHTML = `SHIFTED = ${rainbowColours}`;
+
+//Unshift
+document.getElementById("txt9").innerHTML = rainbowColours.unshift("Lavender");
+document.getElementById("txt10").innerHTML = `UNSHIFTED = ${rainbowColours}`
+
+// length property
+document.getElementById("txt11").innerHTML = rainbowColours[rainbowColours.length] = "GRAY"
+document.getElementById("txt12").innerHTML = ` APPENDING NEW ELEMENT = ${rainbowColours}`
+
+
+//CONCATINATING ARRAYS
+
+//Two arrays
+const warmColors = ["orange", "red", "yellow"];
+const coolColors = ["blue","green", "purple"];
+
+
+const warmXcool = warmColors.concat(coolColors);
+document.querySelector("#txt13").innerHTML = ` CONCATENATED COLORS = ${warmXcool}`
+
+//Three arrays
+const neutralColors = ["black", "white", "gray"];
+
+const allColors = warmColors.concat(coolColors, neutralColors);
+document.querySelector("#txt14").innerHTML = ` CONCATENATING ALL = ${allColors}`
+
+//Array with value
+const newColorArray = allColors.concat("BURGUNDY");
+document.querySelector("#txt15").innerHTML = ` NEW COLOR ARRAY = ${newColorArray}`
+
+
+//Splice
+
+const splicedColors = allColors.splice(2, 0, "Burnt Orange");
+document.querySelector("#txt16").innerHTML = splicedColors;
+document.querySelector("#txt17").innerHTML = `SPLICED COLORS ${splicedColors}`;
