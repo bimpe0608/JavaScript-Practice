45d0490c97da3cfc3a7d64a5f997287fe7f8433a
bimpe <adeyinminuadebimpe@gmail.com>
2023-02-07 11:32:49 +0100

    pop&push

--- Array Methods/script.js
+++ Array Methods/script.js
@@ -10,22 +10,48 @@ console.log(foodTimeTable);
 
 // Filter
 const cheapestFood = foodTimeTable.filter((food) => {
-    return food.price < 500
-})
+  return food.price < 500;
+});
 
-console.log(cheapestFood)
+console.log(cheapestFood);
 
 // Map
 const kindOfFood = foodTimeTable.map((key) => {
-  return key.food
+  return key.food;
 });
 
 console.log(kindOfFood);
 
-
 // Find
 const foodAvailability = foodTimeTable.find((key) => {
-    return key.food === "Swallow"
+  return key.food === "Swallow";
 });
 
 console.log(foodAvailability);
+
+// ARRAY METHODS
+
+//To string
+
+const rainbowColours = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet",
+];
+
+
+document.querySelector("#txt1").innerHTML = rainbowColours.toString();
+
+// Join 
+document.querySelector("#txt2").innerHTML = rainbowColours.join("/");
+
+// pop
+document.querySelector("#txt3").innerHTML = rainbowColours.pop();
+
+//push
+document.querySelector("#txt4").innerHTML = rainbowColours.push();
+
