e4f748d25f213ac22fceba50994b87fa3260260a
bimpe <adeyinminuadebimpe@gmail.com>
2023-02-09 15:59:35 +0100

    multiples

---
+++ JS Dates/script.js
@@ -0,0 +1,14 @@
+//Dates
+
+const num = [1,2,3,4,5,6,7,8,9,10];
+
+const multiples = num.map((data) => {
+ return data * 2; 
+})
+
+console.log(multiples ,multiples [3] = 100)
+
+multiples [3] = 100;
+
+console.log(multiples)
+
